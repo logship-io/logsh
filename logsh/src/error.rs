731d602b80af5e9f6fd817afaed69e70bd58681e
Adam Sulucz <adam.sulucz@gmail.com>
2023-08-23 21:37:26 -0400

    csv

--- logsh/src/error.rs
+++ logsh/src/error.rs
@@ -1,15 +1,32 @@
-use std::{error::Error, fmt};
+use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub struct CliError {
     pub message: String,
     pub code: i32,
 }
 
-impl Error for CliError {}
-
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Error {}: {}", self.code, self.message)
+    }
+}
+
+impl From<std::io::Error> for CliError {
+    fn from(value: std::io::Error) -> Self {
+        Self {
+            message: format!("IO Error: {value}"),
+            code: 1,
+        }
+    }
+}
+
+impl From<csv::Error> for CliError {
+    fn from(value: csv::Error) -> Self {
+        Self {
+            message: format!("CSV Error: {value}"),
+            code: 1,
+        }
     }
 }
